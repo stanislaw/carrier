8b53a85d493ace472c49050bb3e6a7b4e154ae90
stanislaw <s.pankevich@gmail.com>
2011-11-02 06:13:22 +0200

    better assets organization

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,4 @@
+//= require ./autocomplete/jquery.ui.core.min
+//= require ./autocomplete/jquery.ui.widget.min
+//= require ./autocomplete/jquery.ui.position.min
+//= require ./autocomplete/jquery.ui.autocomplete.min
