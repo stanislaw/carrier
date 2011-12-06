567da9bb495d9314e91bb2eb5b3ba12697edbad1
stanislaw <s.pankevich@gmail.com>
2011-12-06 14:19:31 +0200

    remove obsolete assets

--- app/assets/javascripts/autocomplete.js
+++
@@ -1,4 +0,0 @@
-//= require ./autocomplete/jquery.ui.core.min
-//= require ./autocomplete/jquery.ui.widget.min
-//= require ./autocomplete/jquery.ui.position.min
-//= require ./autocomplete/jquery.ui.autocomplete.min
